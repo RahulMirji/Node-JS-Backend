1a5fb7906d22d4f2d6b0220fef184f8519dbdd44
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2025-07-03 21:14:01 +0530

    node js

---
+++ Lecture3/App.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+function requestListener(req, res){
+  console.log(req);
+}
+
+http.createServer(requestListener);
+
+// http.createServer((req, res) =>{
+//   console.log(req);
+// });  // Anonymus function
---
+++ Lecture3/server.js
@@ -0,0 +1,41 @@
+// server.mjs
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  if(req.url === '/'){
+    res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Complete Coding</title></head>');
+  res.write('<body><h1>Welcome to Home </h1></body>');
+  res.write('</html>');
+  return res.end();
+
+  }else if (req.url === '/products'){
+    res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Complete Coding</title></head>');
+  res.write('<body><h1>Checkout my products</h1></body>');
+  res.write('</html>');
+  return res.end();
+
+  }
+  {
+    res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Complete Coding</title></head>');
+  res.write('<body><h1>Bye bye </h1></body>');
+  res.write('</html>');
+  res.end();
+
+  }
+  
+});
+
+// starts a simple http server locally on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
+
+// run with `node server.mjs`
