0d92fd941f351446a242077debedd075b762831c
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2025-07-11 23:43:25 +0530

    today

--- Lecture5/Calculator/app.js
+++ Lecture5/Calculator/app.js
@@ -4,5 +4,5 @@ const server = http.createServer(requestHandler);
 
 const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on address http://localhost:${POR}`);
+  console.log(`Server running on address http://localhost:${PORT}`);
 })
