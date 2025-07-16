d43c8346a4b4afbd58abae4a27004fe003eb4324
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2025-07-16 23:01:13 +0530

    Node

--- Lecture5/Calculator/app.js
+++ Lecture5/Calculator/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const {requestHandler} = require('./handler');
 const server = http.createServer(requestHandler);
 
-const PORT = 3000;
+const PORT = 300;
 server.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
 })
